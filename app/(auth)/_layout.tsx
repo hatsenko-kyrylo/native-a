b4cdf69a170a51acb9eaf749d2cd245f81bd4b64
Hatsenko Kyrylo <gacenko2407@gmail.com>
2024-11-06 17:34:20 +0200

    Finished layout og auth screens. Getting down to logic

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -9,8 +9,8 @@ const AuthLayout = () => {
             <Stack>
                 <Stack.Screen name='sign-in' options={{ headerShown: false }} />
                 <Stack.Screen name='sign-up' options={{ headerShown: false }} />
-                <StatusBar backgroundColor='#161622' style='light' />
             </Stack>
+            <StatusBar backgroundColor='#161622' style='light' />
         </>
     );
 };
--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,24 +1,62 @@
 import { View, Text, ScrollView, Image } from 'react-native';
-import React from 'react';
+import { useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { images } from '@/constants';
+import FormField from '@/components/FormField';
+import CustomButton from '@/components/CustomButton';
+import { Link } from 'expo-router';
 
-const SighIn = () => {
+const SignIn = () => {
+    const [form, setForm] = useState({ email: '', password: '' });
+    const [isSubmitting, setIsSubmitting] = useState(false);
+
+    const submit = () => {};
+
     return (
         <SafeAreaView className='bg-primary h-full'>
             <ScrollView>
-                <View className='w-full h-full justify-center px-4 my-6'>
+                <View className='w-full min-h-[83vh] justify-center px-4 my-6'>
                     <Image
                         source={images.logo}
-                        style={{ resizeMode: 'contain', width: 115, height: 35 }}
+                        resizeMode='contain'
+                        style={{ width: 115, height: 35 }}
                     />
                     <Text className='text-2xl text-white text-semibold font-psemibold mt-10'>
                         Log in to Aora
                     </Text>
+                    <FormField
+                        title='Email'
+                        value={form.email}
+                        handleChangeText={(e: string) => setForm({ ...form, email: e })}
+                        otherStyles='mt-7'
+                        keyboardType='email-address'
+                        placeholder=''
+                    />
+                    <FormField
+                        title='Password'
+                        value={form.password}
+                        handleChangeText={(e: string) => setForm({ ...form, password: e })}
+                        otherStyles='mt-7'
+                        placeholder=''
+                    />
+                    <CustomButton
+                        title='Sign In'
+                        handlePress={submit}
+                        containerStyles='mt-7'
+                        isLoading={isSubmitting}
+                    />
+                    <View className='justify-center pt-5 flex-row gap-2 '>
+                        <Text className='text-lg text-gray-100 font-pregular'>
+                            Don't have an account?
+                        </Text>
+                        <Link href='/sign-up' className='text-lg font-psemibold text-secondary'>
+                            Sign Up
+                        </Link>
+                    </View>
                 </View>
             </ScrollView>
         </SafeAreaView>
     );
 };
 
-export default SighIn;
+export default SignIn;
