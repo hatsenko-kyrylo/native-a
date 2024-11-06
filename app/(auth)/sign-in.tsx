eca2d25f68a310fbd4e81b00e06b21ed2bae738f
Hatsenko Kyrylo <gacenko2407@gmail.com>
2024-11-06 23:22:50 +0200

    Finished Auth logic

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,16 +1,42 @@
-import { View, Text, ScrollView, Image } from 'react-native';
+import { View, Text, ScrollView, Image, Alert } from 'react-native';
 import { useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { Link, router } from 'expo-router';
+
 import { images } from '@/constants';
 import FormField from '@/components/FormField';
 import CustomButton from '@/components/CustomButton';
-import { Link } from 'expo-router';
+import { getCurrentUser, signIn } from '@/lib/appwrite';
+import { useGlobalContext } from '@/context/GlobalProvider';
 
 const SignIn = () => {
     const [form, setForm] = useState({ email: '', password: '' });
     const [isSubmitting, setIsSubmitting] = useState(false);
 
-    const submit = () => {};
+    const { setUser, setIsLoggedIn } = useGlobalContext();
+
+    const submit = async () => {
+        if (!form.email || !form.password) {
+            Alert.alert('Error', 'Please fill in all the fields');
+            return;
+        }
+
+        setIsSubmitting(true);
+
+        try {
+            await signIn(form.email, form.password);
+            const result = await getCurrentUser();
+            setUser(result);
+            setIsLoggedIn(true);
+
+            Alert.alert('Success', 'User signed in successfully');
+            router.replace('/home');
+        } catch (error) {
+            Alert.alert('Error', (error as Error).message);
+        } finally {
+            setIsSubmitting(false);
+        }
+    };
 
     return (
         <SafeAreaView className='bg-primary h-full'>
